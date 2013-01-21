5c18804708a476aa72362b75051736b0992f6a97
Josue G. Gutierrez <josue.gutierrez@mexicoshowroom.com>
2013-01-21 17:50:49 -0600

    Mysql : Adding update, Renaming whatdo.js to whatdocontroller.js , Login preparation

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,20 @@
-angular.module('whatdo', ['angularFilters']).
-  config(['$routeProvider', function($routeProvider) {
+module = angular.module('whatdo', ['angularFilters']);
+module.config(['$routeProvider', function($routeProvider){
   $routeProvider.
       when('/main', {templateUrl: 'partials/main.html',   controller: WhatdoCtrl}).
-      otherwise({redirectTo: '/main'});
-}]);
+      when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl}).
+      otherwise({redirectTo: '/login'});
+	}]);
+module.run( function($rootScope,$location){
+	 $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+      if ( $rootScope.loggedUser == null ) {
+        if ( next.templateUrl == "partials/login.html" ) {
+          // already going to #login, no redirect needed
+        } else {
+          $location.path( "/login" );
+        }
+      }         
+    });
+}
+
+	);
